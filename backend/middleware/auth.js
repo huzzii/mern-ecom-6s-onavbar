c58f49e6a9dcbe631cc3a300521799df8acacc0d
huzzii <huzzii466@gmail.com>
2022-02-09 03:02:09 +0530

    bug fixed error handler

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,4 @@
-const ErrorHandler = require("../utils/ErrorHandler");
+const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncError = require("./catchAsyncError");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
